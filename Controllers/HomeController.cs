e36b42d40c6134d2c0cdbc404bdadad5f9fc5fdc
Azadar <azadarshah3911@gmail.com>
2023-01-22 18:19:38 +0500

    App Init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using ChatGPT.Models;
+using ChatGPT.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatGPT.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IChatGPTService _chatGPTService;
+
+        public HomeController(IChatGPTService chatGPTService, ILogger<HomeController> logger)
+        {
+            _chatGPTService = chatGPTService;
+
+            _logger = logger;
+        }
+
+
+        public IActionResult Index()
+        {
+            Response response = new Response();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(Response response)
+        {
+            if (response.Question != null)
+            {
+                response.ID += 1;
+                ChatGPTModel chat = new ChatGPTModel()
+                {
+                    Token = 1000,
+                    Input = response.Question,
+                    TopP = 0,
+                    Engine = "text-davinci-002",
+                    Temperature = 0.7,
+                    PresencePenalty = 0,
+                    FrequencyPenalty = 0
+                };
+                response.Answer = _chatGPTService.callOpenAi(chat);
+                return View(response);
+            }
+                return View(response);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿/*	$("#formd").on('submit', function (e) {
+	e.preventDefault();
+	var data = $(this).serialize();
+    htm = '<div class="media mb-3"><img src = "https://via.placeholder.com/64x64" class="mr-3 rounded-circle" alt = "..." ><div class="media-body"><h5 class="mt-0">User 1</h5>Hello! How are you?</div></div><div class="media mb-3"><img src="https://via.placeholder.com/64x64" class="mr-3 rounded-circle" alt="..."><div class="media-body"><h5 class="mt-0">User 2</h5>I m good, thanks. How about you?</div></div>';
+	$.ajax({
+		url: "",
+		data: data,
+		success: function (response) {
+			
+			setTimeout(function () {
+
+			},100);
+		}
+	});
+});*/
